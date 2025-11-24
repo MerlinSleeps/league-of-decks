d33848b3a44c95bd333bc8550b7fb252b8bd8562
MerlinSleeps <44499558+MerlinSleeps@users.noreply.github.com>
2025-11-24 15:52:20 +0100

    fixed typo

--- src/components/ui/CardImage.tsx
+++ src/components/ui/CardImage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react'; // Remove useEffect import
+import { useState } from 'react';
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
